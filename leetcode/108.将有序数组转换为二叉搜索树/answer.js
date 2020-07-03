2b2446f31deb566f96e66bf9d5aef274850a2288
RynCh <zekerjc@gmail.com>
2020-07-03 13:59:16 +0800

    20200703

---
+++ leetcode/108.将有序数组转换为二叉搜索树/answer.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    return helper(nums, 0, nums.length - 1);
+};
+
+function helper(nums, left, right) {
+    if (left > right) return null;
+    let mid = Math.floor((left + right) / 2);
+
+    const root = new TreeNode(nums[mid]);
+    root.left = helper(nums, left, mid - 1);
+    root.right = helper(nums, mid + 1, right);
+    return root;
+}
