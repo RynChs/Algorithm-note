7d3fbdd153a254ed0826da0c1da9285e8b3027b0
RynCh <zekerjc@gmail.com>
2020-08-23 20:21:17 +0800

    20200823

---
+++ leetcode/201.数字范围按位与/answer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(m, n) {
+    let shift = 0;
+    // 找到公共前缀
+    while (m < n) {
+        m >>= 1;
+        n >>= 1;
+        ++shift;
+    }
+    return m << shift;
+};
