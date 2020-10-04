a1cc4ee4b2c5e2d06a7a5047cff24c8eecb5e51d
RynCh <zekerjc@gmail.com>
2020-10-04 19:27:10 +0800

    20201004

---
+++ leetcode/dynamic programming/322.零钱兑换/answer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = new Array(amount + 1).fill(amount + 1);
+    dp[0] = 0;
+    for (let i = 0; i < dp.length; i++) {
+        for (const coin of coins) {
+            if (i - coin >= 0) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+    return dp[amount] === amount + 1 ? -1 : dp[amount];
+};
+
+console.log(coinChange([1, 2, 5], 11));
