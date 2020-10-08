5d9f87506fb52542cb9a8f343a8f36d3e56bfde3
RynCh <zekerjc@gmail.com>
2020-10-08 22:07:44 +0800

    20201008

---
+++ leetcode/344.反转字符串/answer.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for (let left = 0, right = s.length - 1 ; left < right; left++, right--) {
+        [s[left], s[right]] = [s[right], s[left]];
+    }
+};
