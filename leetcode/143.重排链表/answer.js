8e24ce330836f1f87fd500829af16e687e555ebe
RynCh <zekerjc@gmail.com>
2020-10-20 17:53:08 +0800

    20201020

---
+++ leetcode/143.重排链表/answer.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    if (head === null) return null;
+    let list = [];
+    let node = head;
+    while (node) {
+        list.push(node);
+        node = node.next;
+    }
+    let i = 0;
+    let j = list.length - 1;
+    while (i < j) {
+        list[i].next = list[j];
+        i++;
+        if (i === j) break;
+        list[j].next = list[i];
+        j--;
+    }
+    list[i].next = null;
+};
