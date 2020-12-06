cf4f35a2ba8d496bc7f7cb269732569f951cc96a
RynCh <zekerjc@gmail.com>
2020-12-06 13:08:02 +0800

    20201206

---
+++ leetcode/202012/118.杨辉三角/answer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const ans = [];
+    for (let i = 0; i < numRows; i++) {
+        const row = new Array(i + 1).fill(1);
+        for (let j = 1; j < i; j++) {
+            row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+        }
+        ans.push(row);
+    }
+    return ans;
+};
+
+console.log(generate(5));
